50302fc0c2b5665047a66fe08d38bf9886ea5530
mateooviedosanchez <mateooviedosanchez@gmail.com>
2025-03-22 18:30:51 -0600

    Dashboard y SSC

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { Response, response } from 'express';
+import { TOKEN_NAME } from './constants/jwt.constants';
+import { Cookies } from './decorator.ts/cookies.decorator';
 
 @ApiTags('Auth')
 @Controller('auth')
@@ -14,9 +17,17 @@ export class AuthController {
   signup(@Body() createUserDto: CreateUserDto){
     this.authService.registerUser(createUserDto)
   }
+
   @Post("login")
-  login(@Body() loginUserDto:LoginUserDto){
-    return this.authService.loginUser(loginUserDto)
+  async login(@Body() loginUserDto:LoginUserDto, @Res({passthrough: true}) response: Response, @Cookies() cookies: any) {
+    const token = await this.authService.loginUser(loginUserDto)
+    response.cookie(TOKEN_NAME, token, {
+      httpOnly: false,
+      secure: true,
+      sameSite: 'none',
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
+    return;
   }
 
   @Patch("/:email")
--- src/auth/constants/jwt.constants.ts
+++ src/auth/constants/jwt.constants.ts
@@ -1,2 +1,3 @@
 export const JWT_KEY = "kjdhgskjr42KSJF@#$";
-export const EXPIRES_IN = '120s'
+export const EXPIRES_IN = '7d';
+export const TOKEN_NAME = 'auth_for_ocso';
