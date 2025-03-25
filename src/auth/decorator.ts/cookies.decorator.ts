c7bfba4e8bd78a335571c9e88b659945f02523a7
mateooviedosanchez <mateooviedosanchez@gmail.com>
2025-03-25 15:51:41 -0600

    feat: Cookies in back

--- src/auth/decorator.ts/cookies.decorator.ts
+++ src/auth/decorator.ts/cookies.decorator.ts
@@ -4,3 +4,5 @@ export const Cookies = createParamDecorator((data: string, ctx: ExecutionContext
   const request = ctx.switchToHttp().getRequest();
   return data ? request.cookies?.[data] : request.cookies;
 });
+
+//commit back
